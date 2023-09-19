8f45ffe5622425ac36e47f2748dbe2b348297c4f
Sergey <serg311091@gmail.com>
2023-09-19 19:28:54 +0300

    Добавлена задача на нахождение макс и мин чисел

--- Task002/Program.cs
+++ Task002/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2. 
+// Напишите программу котораяя на вход принимает два числа и выдаёт какое число большее а какое меньшее.
+// Например:
+	// a = 5; b = 7; -> max = 7
+	// a = 2; b = 10; -> max = 10
+	// a = -9; b = -3; -> max = -3
+Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+int min = 0;
+if (number1 > number2){
+max = number1;
+min = number2;
+Console.WriteLine($"max -> {max}");
+Console.WriteLine($"min -> {min}");
+}
+
+else if (number2 > number1 ) {
+    max = number2;
+    min = number1;
+    Console.WriteLine($"max -> {max}");
+    Console.WriteLine($"min -> {min}");
+}
+else if (number1 == number2) {
+    Console.WriteLine($"{number1} и {number2} равны");
+}
+else {
+    Console.WriteLine("Вы ввели неверные данные");
+}
+
